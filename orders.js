79e26594c27114d5d593586c144f989d53ef99d1
Prosper <prosperojo2@gmail.com>
2025-03-02 06:46:58 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856721',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Square 101 Drone',
+        productNumber: '856722',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Delta UV Drone',
+        productNumber: '856723',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'LVT Cam Drone',
+        productNumber: '856724',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'NST 111 Series Drone',
+        productNumber: '856725',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Square 101 Drone',
+        productNumber: '856756',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
